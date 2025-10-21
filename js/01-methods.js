049952bffbc9a12af6225e85a850fa5c9a68e63a
Olexiy-repin <repinlyoha@gmail.com>
2025-10-21 13:02:47 +0300

    classwork

---
+++ js/01-methods.js
@@ -0,0 +1,78 @@
+/*
+ * Методи рядків
+ *
+ *  - slice
+ *  - toLowerCase/toUpperCase
+ *  - includes
+ *  - startsWith/endsWith
+ *  - indexOf/lastIndexOf
+ *  - trim
+ *  - padStart, padEnd
+ */
+
+// const firstName = 'Oleksii';
+// const firstNameCopy = firstName.slice(1);
+
+// console.log('firstName:', firstName);
+// console.log('firstNameCopy:', firstNameCopy);
+
+// const email = 'Test@gmail.com';
+// const userEmail = 'test@gmail.com';
+
+// console.log(email.toLowerCase() === userEmail.toLowerCase());
+
+// const email = 'test@gmail.com';
+
+// console.log(email.includes('@'));
+
+// const email = 'test@gmail.com';
+
+// console.log(email.endsWith('.com'));
+
+// const email = 'test@gmail.com';
+// const indexOfDomen = email.indexOf('@');
+// const domen = email.slice(indexOfDomen + 1);
+
+// console.log('email =', email);
+// console.log('indexOfDomen =', indexOfDomen);
+// console.log('domen =', domen);
+
+// const filmTitle = '  Harry Potter  ';
+// const normalizedFilmTitle = filmTitle.trim();
+
+// console.log('filmTitle:', filmTitle);
+// console.log('normalizedFilmTitle:', normalizedFilmTitle);
+
+// let days = 1;
+// let hours = 2;
+// let minutes = 20;
+// let seconds = 8;
+
+// if (days < 10) {
+//   days = '0' + days;
+// }
+
+// if (hours < 10) {
+//   hours = '0' + hours;
+// }
+
+// if (minutes < 10) {
+//   minutes = '0' + minutes;
+// }
+
+// if (seconds < 10) {
+//   seconds = '0' + seconds;
+// }
+
+// console.log(`${days}:${hours}:${minutes}:${seconds}`);
+
+// function pad(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+// const days = pad(1);
+// const hours = pad(2);
+// const minutes = pad(20);
+// const seconds = pad(8);
+
+// console.log(`${days}:${hours}:${minutes}:${seconds}`);
